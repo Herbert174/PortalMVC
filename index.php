3966113c425440fd7948af7263733c5f382aaaf0
Herbert Santos <herbertms174@gmail.com>
2022-12-04 14:36:33 -0300

    Adicionando projeto inicial do php

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Titulo -->
+        <title>Sidebar Dashboard</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <!-- CSS -->
+        <link rel="stylesheet" href="estilo.css">
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" charset="utf-8"></script>
+    </head>
+
+    
+    <body>
+        <input type="checkbox" id="check">
+        <!-- Header area start -->
+        <header>
+            <label for="check">
+                <i class="fas fa-bars" id="sidebar_btn"></i>
+            </label>
+            <div class="left_area">
+                <h3>Coding <span>Snow</span></h3>
+            </div>
+            <div class="right_area">
+                <a href="#" class="logout_btn">Login</a>
+            </div>
+            <div class="right_area">
+                <a href="#" class="logout_btn">Cadastro</a>
+            </div>
+        </header>
+        <!-- Header area end -->
+
+        <!-- Mobile navigation bar start -->
+        <div class="mobile_nav">
+            <div class="nav_bar">
+                <a href="pagina_usuario.html"><img src="imagens/perfil.jpg" class="mobile_profile_image" alt=""></a>
+                <i class="fa fa-bars nav_btn"></i>
+            </div>
+            <div class="mobile_nav_items">
+                <a href="#"><i class="fas fa-desktop"></i><span>Home</span></a>
+                <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+                <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+                <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
+                <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
+                <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+            </div>
+        </div>
+        <!-- Mobile navigation bar end -->
+
+        <!-- Sidebar start -->
+        <div class="sidebar">
+            <div class="profile_info">
+                <a class="link_foto" href="pagina_usuario.php"><img src="imagens/perfil.jpg" class="profile_image" alt=""></a>
+                <h4>Herbert</h4>
+            </div>
+            <a href="#"><i class="fas fa-desktop"></i><span>Home</span></a>
+            <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+            <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+            <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
+            <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
+            <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+        </div>
+        <!-- Sidebar end -->
+
+        <section class="content">
+            <div class="Container">
+                <div class="col-sm-9">
+                    <div class="row custom">
+                        <div class="col-sm-3">
+                            <img src="imagens/Postagem1.png" class="img_postagem">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="row">
+                                <span class="negrito">Criando o seu personagem</span>
+                            </div>
+                            <div class="row">
+                                <p>A construção de personagens é baseada nos jogos da série The Elder Scrolls, 
+                                    com bastante liberdade para o jogador criar o personagem da forma que preferir.
+                                    <a href="noticias1.php">leia mais...</a>
+                                </p>
+                            </div>
+                            <div class="row">
+                                Comentarios: Eso é um jogo fantastico
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row custom">
+                        <div class="col-sm-3">
+                            <img src="imagens/Postagem1.png" class="img_postagem">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="row">
+                                <span class="negrito">Criando o seu personagem</span>
+                            </div>
+                            <div class="row">
+                                <p>A construção de personagens é baseada nos jogos da série The Elder Scrolls, 
+                                    com bastante liberdade para o jogador criar o personagem da forma que preferir.
+                                    <a href="noticias1.php">leia mais...</a>
+                                </p>
+                            </div>
+                            <div class="row">
+                                Comentarios: Eso é um jogo fantastico
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row custom">
+                        <div class="col-sm-3">
+                            <img src="imagens/Postagem1.png" class="img_postagem">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="row">
+                                <span class="negrito">Criando o seu personagem</span>
+                            </div>
+                            <div class="row">
+                                <p>A construção de personagens é baseada nos jogos da série The Elder Scrolls, 
+                                    com bastante liberdade para o jogador criar o personagem da forma que preferir.
+                                    <a href="noticias1.php">leia mais...</a>
+                                </p>
+                            </div>
+                            <div class="row">
+                                Comentarios: Eso é um jogo fantastico
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row custom">
+                        <div class="col-sm-3">
+                            <img src="imagens/Postagem1.png" class="img_postagem">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="row">
+                                <span class="negrito">Criando o seu personagem</span>
+                            </div>
+                            <div class="row">
+                                <p>A construção de personagens é baseada nos jogos da série The Elder Scrolls, 
+                                    com bastante liberdade para o jogador criar o personagem da forma que preferir.
+                                    <a href="noticias1.php">leia mais...</a>
+                                </p>
+                            </div>
+                            <div class="row">
+                                Comentarios: Eso é um jogo fantastico
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row custom">
+                        <div class="col-sm-3">
+                            <img src="imagens/Postagem1.png" class="img_postagem">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="row">
+                                <span class="negrito">Criando o seu personagem</span>
+                            </div>
+                            <div class="row">
+                                <p>A construção de personagens é baseada nos jogos da série The Elder Scrolls, 
+                                    com bastante liberdade para o jogador criar o personagem da forma que preferir.
+                                    <a href="noticias1.php">leia mais...</a>
+                                </p>
+                            </div>
+                            <div class="row">
+                                Comentarios: Eso é um jogo fantastico
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-1"></div>
+                <div class="col-sm-2">
+                    <div class="row custom">
+                        <img class="img_noticia" src="imagens/logo.png">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>-->
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.nav_btn').click(function(){
+                    $('.mobile_nav_items').toggleClass('active');
+                    })
+                });
+        </script>
+
+    </body>
+</html>
---
+++ noticias1.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Titulo -->
+        <title>Sidebar Dashboard</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <!-- CSS -->
+        <link rel="stylesheet" href="estilo.css">
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" charset="utf-8"></script>
+    </head>
+
+    
+    <body>
+        <input type="checkbox" id="check">
+        <!-- Header area start -->
+        <header>
+            <label for="check">
+                <i class="fas fa-bars" id="sidebar_btn"></i>
+            </label>
+            <div class="left_area">
+                <h3>Coding <span>Snow</span></h3>
+            </div>
+            <div class="right_area">
+                <a href="#" class="logout_btn">Logout</a>
+            </div>
+            <div class="right_area">
+                <a href="#" class="logout_btn">Login</a>
+            </div>
+        </header>
+        <!-- Header area end -->
+
+        <!-- Mobile navigation bar start -->
+        <div class="mobile_nav">
+            <div class="nav_bar">
+                <a href="pagina_usuario.html"><img src="imagens/perfil.jpg" class="mobile_profile_image" alt=""></a>
+                <i class="fa fa-bars nav_btn"></i>
+            </div>
+            <div class="mobile_nav_items">
+                <a href="index.php"><i class="fas fa-desktop"></i><span>Home</span></a>
+                <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+                <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+                <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
+                <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
+                <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+            </div>
+        </div>
+        <!-- Mobile navigation bar end -->
+
+        <!-- Sidebar start -->
+        <div class="sidebar">
+            <div class="profile_info">
+                <a class="link_foto" href="pagina_usuario.php"><img src="imagens/perfil.jpg" class="profile_image" alt=""></a>
+                <h4>Herbert</h4>
+            </div>
+            <a href="index.php"><i class="fas fa-desktop"></i><span>Home</span></a>
+            <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+            <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+            <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
+            <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
+            <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+        </div>
+        <!-- Sidebar end -->
+
+        <section class="content">
+            <div class="Container">
+                <div class="col-sm-9">
+                    <div class="row custom">
+                        <div class="page-header texto-capa">
+                            <h1>Elder Scrolls Online</h1>
+                        </div>
+                  
+                        <section class="conteudo">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="col-md-12"> 
+                                            <h2 class="texto-capa">The Elder Scrolls Online: aprenda a jogar o esperado game da série</h2>                
+                                            <p>The Elder Scrolls Online é o esperado MMORPG no mundo de The Elder Scrolls que leva os jogadores ao enorme continente de Tamriel. Assim como o jogo da série, Skyrim, os jogadores precisam criar e treinar um personagem para sobreviver as missões e criaturas do jogo. Confira como jogar o título lançado para Windows e Mac.</p><br>
+                                            <h3>Como começar</h3>
+                                            <p>Ao abrir o jogo, clique em “Play” para inciar a tela principal. Conecte com sua conta para abrir o menu de criação de personagens. Lembrando que o jogo não possui legendas em português, então é preciso um conhecimento básico de leitura em inglês para aproveitar sua experiência em The Elder Scrolls Online.</p><br>
+                                        </div>
+                                    </div>
+                                </div><!--row-->
+                                <div class="row row-eso">
+                                    <div class="col-md-12">
+                                        <div class="col-md-12">
+                                            <img class="img-responsive img_postagem1" src="imagens/Postagem5.png">
+                                            <h3>Criando o seu personagem</h3>
+                                            <p>A construção de personagens é baseada nos jogos da série The Elder Scrolls, com bastante liberdade para o jogador criar o personagem da forma que preferir. Antes de começar, você precisa construir seu personagem com base nas raças disponíveis, entre elas os Breton, Altmer, Dunmer, Redguard, Argonia, entre outras, cada uma com sua base de aliança própria, entre as três facções presentes – Daggerfall Covenant, Ebonheart Pact e Aldmeri Dominion.</p><br>
+                                            <p>A escolha da facção determinará o local onde você irá começar o jogo. Fique atento para as raças de classes, pois cada uma possui bônus diferentes e que podem ser úteis para o seu personagem durante o jogo. Por fim, basta seguir as abas de criação e selecionar as opções que preferir para o seu personagem, escolher um nome e clicar em “Create” para começar a jogar.</p><a href="art1Eso.html">leia mais...</a><br><br><br>
+                                        </div>
+                                    </div>
+                                </div><!--row-->
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="col-md-12">
+                                            <h1 class="texto-capa">Guia: Endeavors – Novo sistema de recompensa do ESO</h1>
+                                            <p>Como parte do Update 30 que sairá no meio de Jun/21, o novo sistema de Endeavors que dará aos jogadores a oportunidade de conseguir itens de Caixas de Crown.</p>
+                                            <p>Este novo sistema de “Recompensas por Esforços” do jogador, virá como parte do conteúdo do jogo base. Com este novo sistema você poderá completar tarefas diárias e semanais conhecidas como “Endeavors”, onde você pode conseguir, além do Ouro e Experiência, os conhecidos “Seals of Endeavor”.</p>
+                                            <p>Este sistema irá funcionar da seguinte maneira: Quando você conecta no jogo, você irá automaticamente receber várias missões diárias e semanais de Endeavors, que irão solicitar que você realize diversas tarefas, como:</p>
+                                            <ul>
+                                                <li>Roubar ou Bater Carteira</li>
+                                                <li>Completar Missões</li>
+                                                <li>Derrotar inimigos usando habilidades de Classe ou Arma</li>
+                                                <li>Vender itens para vendedores</li>
+                                                <li>Fabricar tipos diferentes de itens</li>
+                                                <li>Derrotar tipos diferentes de monstros</li>
+                                                <li>Colher nodos de recursos</li>
+                                                <li>E muito mais!</li>
+                                            </ul>
+                                            <img class="img-responsive img_postagem1" src="imagens/postagem14.png"><br>
+                                            <p>Estas são apenas algumas das atividades que você pode ser solicitado a fazer para completar seus “esforços”. Frequentemente, serão coisas que você já costuma fazer em suas aventuras no ESO.</p>
+                                            <br>
+                                            <a href="#">leia mais...</a>
+                                        </div>
+                                    </div> 
+                                </div><!--row-->
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                <div class="col-sm-1"></div>
+                <div class="col-sm-2">
+                    <div class="row custom">
+                        <img class="img_noticia" src="imagens/logo.png">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>-->
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.nav_btn').click(function(){
+                    $('.mobile_nav_items').toggleClass('active');
+                    })
+                });
+        </script>
+
+    </body>
+</html>
